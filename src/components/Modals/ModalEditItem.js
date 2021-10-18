fd23a626b64733fd0be12e3fcc38cc2d2ac8f1d2
Rizqi Pratama <rev4rmy@gmail.com>
2021-10-18 14:58:03 +0700

    edit data cy

--- src/components/Modals/ModalEditItem.js
+++ src/components/Modals/ModalEditItem.js
@@ -69,7 +69,10 @@ function ModalEditItem({ show, handleClose, title, text, editedItem }) {
   }, [show]);
 
   const formatOptionLabel = ({ value, label }) => (
-    <div className="d-flex align-items-center">
+    <div
+      data-cy="modal-add-priority-item"
+      className="d-flex align-items-center"
+    >
       <div className={`label-indicator ${value}`}></div>
       <div>{label}</div>
     </div>
